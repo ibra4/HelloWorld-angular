27001e4293453a1dca4e35f849d60f8ef7d7fa8d
ibra4 <ibra16@itc.hu.edu.jo>
2021-02-26 22:54:58 +0200

    Test module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HelloWorld';
+
+  public title : string;
+
+  ngOnInit() {
+    this.title = 'hi'
+  }
+
+  greet(name : string) {
+    return "hi " + name
+  }
 }
